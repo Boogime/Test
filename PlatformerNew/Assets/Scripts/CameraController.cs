5457700e6b2d20d8d9fe0ed44497be97a8ab9c87
Boogime <sokirkotop45684@bk.ru>
2021-02-14 00:01:32 +0300

    Test

---
+++ PlatformerNew/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform camera;
+    public Transform target;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float value = 0.01f;
+    private void Update()
+    {
+        if (Vector2.Distance(camera.position, target.position) > value)
+        {
+            camera.position = new Vector3(Mathf.Lerp(camera.position.x, target.position.x, speed * Time.deltaTime),
+              Mathf.Lerp(camera.position.y, target.position.y, speed * Time.deltaTime), -10);
+        }
+    }
+}
+
---
+++ PlatformerNew/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private enum PlayerState
+    {
+        Ground,
+        Jump
+    }
+
+    [SerializeField] private float Speed = 500f;
+    [SerializeField] private float JumpForce = 500f;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+
+    private PlayerState сurrentState;
+    private Vector2 input;
+    private Vector2 prevInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void SetState(PlayerState newState)
+    {
+        if (Equals(newState, сurrentState))
+            return;
+        сurrentState = newState;
+        switch (сurrentState)
+        {
+            case PlayerState.Ground:
+                break;
+            case PlayerState.Jump:
+                rb.AddForce(Vector2.up * JumpForce);
+                break;
+        }
+    }
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = input.x < 0;
+
+        animator.SetFloat("speedX", Mathf.Abs(input.x));
+        animator.SetBool("jump", сurrentState == PlayerState.Jump);
+        if (Input.GetKeyDown(KeyCode.Space) && сurrentState == PlayerState.Ground)
+        {
+            SetState(PlayerState.Jump);
+        } 
+    }
+    private void FixedUpdate()
+    {
+        var vel = rb.velocity;
+        vel.x = input.x * Time.fixedDeltaTime * Speed;
+        rb.velocity = vel;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            SetState(PlayerState.Ground);
+        }
+    }
+
+
+
+
+
+
+
+}
